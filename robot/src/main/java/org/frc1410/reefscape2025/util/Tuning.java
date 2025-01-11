76cf9f07bda45ed898447059fdf3d373f4268108
Mexican1oreo <Anthonyjeuc1234@gmail.com>
2025-01-11 12:44:24 -0700

    Removed feedforward and changed swerve drive PID (not fully tuned)

--- robot/src/main/java/org/frc1410/reefscape2025/util/Tuning.java
+++ robot/src/main/java/org/frc1410/reefscape2025/util/Tuning.java
@@ -5,16 +5,18 @@ import com.pathplanner.lib.config.PIDConstants;
 public final class Tuning {
 
     // Drivetrain
-    public static final double SWERVE_DRIVE_P = 0;
+    public static final double SWERVE_DRIVE_P = 0.401;
     public static final double SWERVE_DRIVE_I = 0;
-    public static final double SWERVE_DRIVE_D = 0;
+    public static final double SWERVE_DRIVE_D = -0.015;
 
     public static final double SWERVE_STEER_P = 2;
     public static final double SWERVE_STEER_I = 0;
     public static final double SWERVE_STEER_D = 0;
 
-    public static final double DRIVE_KS = 0.31720;
-    public static final double DRIVE_KV = 0.12280;
+//    public static final double DRIVE_KS = 0.31720;
+//    public static final double DRIVE_KV = 0.12280;
+    public static final double DRIVE_KS = 0;
+    public static final double DRIVE_KV = 0;
 
     // Path following
     public static final PIDConstants PATH_AUTO_TRANSLATION_CONSTRAINTS = new PIDConstants(3.5, 0, 0.8);
