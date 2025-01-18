81334769fef94d1f9d14510ec33202a5d8f07c7a
Calstar9000 <Calstar9000+github@gmail.com>
2025-01-18 14:46:34 -0700

    New colors for elevator states (Can be changed)

--- robot/src/main/java/org/frc1410/reefscape2025/subsystems/LEDs.java
+++ robot/src/main/java/org/frc1410/reefscape2025/subsystems/LEDs.java
@@ -23,20 +23,26 @@ public class LEDs {
 
     public void setColor(Color color) {
         switch (color) {
-            case PRANCING_PONY_PINK -> this.candle.setLEDs(247,2,174);
-            case LIMELIGHT_GREEN -> this.candle.setLEDs(10,247,2);
+            case GOT_PIECE_GREEN -> this.candle.setLEDs(10,247,2);
+            case LEVEL_ONE_PINK -> this.candle.setLEDs(247,2,174);
+            case LEVEL_TWO_VIOLET -> this.candle.setLEDs(146,28,189);
+            case LEVEL_THREE_CYAN -> this.candle.setLEDs(30, 250, 224);
+            case LEVEL_FOUR_YELLOW -> this.candle.setLEDs(250, 239, 30);
+            case LEVEL_FIVE_DEEP_BLUE -> this.candle.setLEDs(24, 24, 214);
             case STOLEN_LIFEBLOOD_RED -> this.candle.animate(new StrobeAnimation(255,18,26));
-            case VIVACIOUS_VIOLENT_VIOLET -> this.candle.setLEDs(146,28,189);
             case CLIMBING_RAINBOW_ANIMATION -> this.candle.animate(new RainbowAnimation());
             case OCEAN_BREEZE -> this.candle.setLEDs(30, 100, 250);
         }
     }
 
     public enum Color {
+        GOT_PIECE_GREEN,
+        LEVEL_ONE_PINK,
+        LEVEL_TWO_VIOLET,
+        LEVEL_THREE_CYAN,
+        LEVEL_FOUR_YELLOW,
+        LEVEL_FIVE_DEEP_BLUE,
         STOLEN_LIFEBLOOD_RED,
-        LIMELIGHT_GREEN,
-        PRANCING_PONY_PINK,
-        VIVACIOUS_VIOLENT_VIOLET,
         OCEAN_BREEZE,
         CLIMBING_RAINBOW_ANIMATION,
     }
