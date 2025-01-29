5888ff0ec8988dc0503a876f610a2a46ec01a9a2
Samuel Rojas <samprogram7@gmail.com>
2025-01-29 16:02:20 -0700

    Elevator Started code

---
+++ robot/src/main/java/org/frc1410/reefscape2025/subsystems/ElevatorState.java
@@ -0,0 +1,14 @@
+package org.frc1410.reefscape2025.subsystems;
+
+public enum ElevatorState {
+    LEVELONE,
+    LEVELTWO,
+    LEVELTHREE,
+    LEVELFOUR,
+    INTAKE_POSITION
+    //comment
+}
+
+
+
+
