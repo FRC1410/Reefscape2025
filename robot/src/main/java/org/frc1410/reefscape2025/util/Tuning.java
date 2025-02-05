d82aea08bc9761979a809138dfe9c79fa2803505
Mexican1oreo <Anthonyjeuc1234@gmail.com>
2025-02-04 23:53:37 -0700

    got rid of some tuning values *I think* we dont need

--- robot/src/main/java/org/frc1410/reefscape2025/util/Tuning.java
+++ robot/src/main/java/org/frc1410/reefscape2025/util/Tuning.java
@@ -2,13 +2,6 @@ package org.frc1410.reefscape2025.util;
 
 public final class Tuning {
     // Elevator
-    public static double LEFT_ELEVATOR_MOTOR_P = 1;
-    public static double LEFT_ELEVATOR_MOTOR_I = 0;
-    public static double LEFT_ELEVATOR_MOTOR_D = 0;
-
-    public static double RIGHT_ELEVATOR_MOTOR_P = 1;
-    public static double RIGHT_ELEVATOR_MOTOR_I = 0;
-    public static double RIGHT_ELEVATOR_MOTOR_D = 0;
 
     public static double ELEVEATOR_P = 1;
     public static double ELEVEATOR_I = 1;
